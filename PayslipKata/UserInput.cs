b72ebd9b61e27992c4115e63efa0c7c171763e9e
GorginZ <georgia.z.leng@gmail.com>
2020-09-06 13:54:05 +1000

    UserInput Property validations.

--- PayslipKata/UserInput.cs
+++ PayslipKata/UserInput.cs
@@ -1,26 +1,44 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 namespace PayslipKata
 {
   public class UserInput
   {
-    //all types because all user input will be strings.
-    //this also means it will be easier to validate and re-prompt in instance of invalid or wrong input
+    //all user input is strings, can perform validation here on UserInput properties and easily re-prompt
+    [Required(ErrorMessage = "First Name Required")]
+    [MaxLength(15, ErrorMessage = "First Name should not more than 1`5 character")]
+    [MinLength(3, ErrorMessage = "First Name should be more than 2 characters")]
     public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last Name Required")]
+    [MaxLength(15, ErrorMessage = "Last Name should not more than 1`5 character")]
+    [MinLength(2, ErrorMessage = "Last Name should be more than 2 characters")]
     public string SurName { get; set; }
+
+    [Required]
+    [RegularExpression("[0-9]", ErrorMessage = "Annual Salary must include numbers only")]
     public string AnnualSalary { get; set; }
+    [Required]
+    [RegularExpression("[0-9]", ErrorMessage = "Your super rate must include numbers only")]
     public string SuperRate { get; set; }
+
+    [Required]
+    // [RegularExpression("^[0-9]+$", ErrorMessage = "Your work start your should only include numbers")]
     public string PaymentStart { get; set; }
+    [Required]
+    // [RegularExpression("^[0-9]+$", ErrorMessage = "Your work start your should only include numbers")] 
     public string PaymentEnd { get; set; }
     //constructor
-    public UserInput(string firstName, string surName, string annualSalary, string superRate, string paymentStart,string paymentEnd)
+    public UserInput(string firstName, string surName, string annualSalary, string superRate, string paymentStart, string paymentEnd)
     {
-        FirstName = firstName;
-         SurName = surName;
-         AnnualSalary = annualSalary;
-         SuperRate = superRate;
-         PaymentStart = paymentStart;
-         PaymentEnd = paymentEnd;
-        }
+      FirstName = GetFirstName();
+      SurName = GetSurName();
+      AnnualSalary = GetAnnualSalary();
+      SuperRate = GetSuperRate();
+      PaymentStart = GetPaymentStart();
+      PaymentEnd = GetPaymentEnd();
+    }
 
     public string GetFirstName()
     {
@@ -49,14 +67,14 @@ namespace PayslipKata
       return superRate;
     }
 
-     public string GetPaymentStart()
+    public string GetPaymentStart()
     {
       Console.WriteLine("Please enter the payment start date:");
       var paymentStart = Console.ReadLine();
       return paymentStart;
     }
 
-       public string GetPaymentEnd()
+    public string GetPaymentEnd()
     {
       Console.WriteLine("Please enter the payment end date");
       var paymentEnd = Console.ReadLine();
